9d9aaa8196df9f97463cef64b0d47a490f446452
Sinan ACAR <sinanacar14654@gmail.com>
2017-07-17 23:04:44 +0300

    Add files via upload

---
+++ Sayının Karesini bulma programı/Sayının Karesini bulma programı/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double sayı;
+
+            sayı = Convert.ToDouble(textBox1.Text);
+            sayı = sayı * sayı;
+            lblsonuc.Text = sayı.ToString();
+
+        }
+    }
+}
